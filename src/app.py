110b89037e3975fe6e1f2e673759892298075ac1
Anton <antonkravtsevich@gmail.com>
2018-04-02 12:15:38 +0300

    fix cors

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_corse import CORS
+from flask_cors import CORS
 import sentiment_processing as sp
 from temp_storage import TempStorage
 from datetime import datetime
