0348b5ab8550881c79a8b543b0632f5b8f6a1f2a
Anton <antonkravtsevich@gmail.com>
2018-04-26 00:54:01 +0300

    fix tests

--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 import unittest
-import src.sentiment_processing as sp
+from src.sentiment_processing import SentimentProcessor
+
 
 class TestSentimentProcessing(unittest.TestCase):
 
     def test_type(self):
+        sp = SentimentProcessor()
         result = sp.get_polarity('hello world')
         self.assertEqual(type(result), float)
 
     def test_range(self):
+        sp = SentimentProcessor()
         result = sp.get_polarity('hello world')
         self.assertGreater(result, -1)
         self.assertLess(result, 1)
 
     def test_positive(self):
+        sp = SentimentProcessor()
         text = 'happy good nice cool'
         result = sp.get_polarity(text)
         self.assertGreater(result, 0)
 
     def test_negative(self):
+        sp = SentimentProcessor()
         text = 'ugly bad worse aufull terrible'
         result = sp.get_polarity(text)
         self.assertLess(result, 0)
